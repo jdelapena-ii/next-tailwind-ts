6f9d309bb6250914cb4ed3538baf8cc164c42fe2
Jedo <jtdelapenaii@gmail.com>
2021-05-18 00:05:52 +0800

    enclosed each skill icon in a div tag

--- components/skills.tsx
+++ components/skills.tsx
@@ -15,18 +15,42 @@ export default function Skills() {
     return (
         <section className="max-w-6xl min-h-screen m-auto" id="about">
             <div className="min-h-screen grid-cols-6 grid-rows-3 gap-x-16 gap-y-12 justify-items-center md:grid">
-                <HtmlIcon />
-                <CssIcon />
-                <JSIcon />
-                <ReactIcon />
-                <ReduxIcon />
-                <TypeScriptIcon />
-                <NextIcon />
-                <GatsbyIcon />
-                <GraphqlIcon />
-                <GitIcon />
-                <GithubIcon />
-                <LinuxIcon />
+                <div className="w-24 h-24 transition duration-500 ease-in-out transform hover:scale-125">
+                    <HtmlIcon />
+                </div>
+                <div className="w-24 h-24 transition duration-500 ease-in-out transform hover:scale-125">
+                    <CssIcon />
+                </div>
+                <div className="w-24 h-24 transition duration-500 ease-in-out transform hover:scale-125">
+                    <JSIcon />
+                </div>
+                <div className="w-24 h-24 transition duration-500 ease-in-out transform hover:scale-125">
+                    <ReactIcon />
+                </div>
+                <div className="w-24 h-24 transition duration-500 ease-in-out transform hover:scale-125">
+                    <ReduxIcon />
+                </div>
+                <div className="w-24 h-24 transition duration-500 ease-in-out transform hover:scale-125">
+                    <TypeScriptIcon />
+                </div>
+                <div className="w-24 h-24 transition duration-500 ease-in-out transform hover:scale-125">
+                    <NextIcon />
+                </div>
+                <div className="w-24 h-24 transition duration-500 ease-in-out transform hover:scale-125">
+                    <GatsbyIcon />
+                </div>
+                <div className="w-24 h-24 transition duration-500 ease-in-out transform hover:scale-125">
+                    <GraphqlIcon /> 
+                </div>
+                <div className="w-24 h-24 transition duration-500 ease-in-out transform hover:scale-125">   
+                    <GitIcon />
+                </div>
+                <div className="w-24 h-24 transition duration-500 ease-in-out transform hover:scale-125">
+                        <GithubIcon />
+                </div>
+                <div className="w-24 h-24 transition duration-500 ease-in-out transform hover:scale-125">
+                        <LinuxIcon />
+                </div>
             </div>
         </section>    
     )
