e9c3c5e2f594d7ce45531966585f794710505b4c
root <root@Jedo.localdomain>
2021-11-14 00:51:18 +0800

    Test commit

---
+++ components/test.js
@@ -0,0 +1,23 @@
+import Image from 'next/image'
+
+export default function TestSection() {
+    return (
+      <main className="relative">
+        <div style={ {zIndex: -1} }
+        className="absolute object-cover w-full min-h-screen overflow-hidden">
+          <Image
+            alt="computer-bg"
+            src="/computer-hero.jpg"
+            layout="fill"
+            objectFit="cover"
+            quality={100}
+          />
+        </div>
+        <p className="p-40 text-5xl antialiased leading-10 text-center text-gray-200 text-align">
+          Image Component
+          <br />
+          as a Background
+        </p>
+      </main>
+    )
+  }
